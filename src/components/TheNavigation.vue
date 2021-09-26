44ef61a4f4295b84bcf168caaa1ce0171f11c701
Leonardo Giraldi <leogiraldimg@gmail.com>
2021-09-26 11:56:43 -0300

    first commit

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="nav">
+    <h1>GloboShop</h1>
+    <router-link to="/items">Items</router-link>
+    <router-link to="/about">About us</router-link>
+  </div>
+</template>
+
+<script>
+import { useStore } from "vuex";
+import { computed } from "vue";
+
+export default {
+  setup() {
+    const store = useStore();
+    const numOfitemsInCart = computed(
+      () => store.state.numOfitemsInCart
+    );
+
+    return {
+      numOfitemsInCart
+    };
+  }
+};
+</script>
+
+<style scoped>
+.nav {
+  overflow: hidden;
+  background-color: #1d2063;
+  margin: 1% 20%;
+  height: 5rem;
+  display: flex;
+  align-items: center;
+  border-radius: 6px;
+  padding: 1%;
+}
+
+.nav h1 {
+  color: white;
+  margin: 5% 5%;
+  user-select: none;
+}
+
+.nav a {
+  color: #f2f2f2;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 18px;
+  margin: 10px;
+  white-space: nowrap;
+  overflow: hidden;
+}
+
+.nav a:hover {
+  background-color: #ddd;
+  color: black;
+  border-radius: 22px;
+  transition: background-color 0.3s ease-out 20ms;
+}
+
+.nav a.router-link-active {
+  background-color: #006bff;
+  color: white;
+  border-radius: 22px;
+}
+
+@media screen and (max-width: 1180px) {
+  .nav {
+    display: block;
+    height: 100%;
+  }
+
+  .nav a {
+    display: block;
+  }
+}
+</style>
