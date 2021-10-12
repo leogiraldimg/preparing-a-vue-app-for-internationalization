c79a597463840b5babd947028b38752a8d79405f
leogiraldimg@gmail.com <leogiraldimg@gmail.com>
2021-10-12 18:48:48 -0300

    :sparkles: handles locales with region

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -4,15 +4,36 @@ import numberFormats from './numberFormats.js';
 import datetimeFormats from './datetimeFormats.js';
 import pluralRules from './pluralRules.js';
 
-const DEFAULT_LOCALE = 'hr';
+const DEFAULT_LOCALE = 'en';
+const extractLanguage = locale => {
+  return locale.split('-')[0];
+};
 
 function getDefaultLocale() {
   const availableLocales = Reflect.ownKeys(messages);
   const navigatorLocale = navigator.language;
 
-  return availableLocales.includes(navigatorLocale)
-    ? navigatorLocale
-    : DEFAULT_LOCALE;
+  if (availableLocales.includes(navigatorLocale)) {
+    return navigatorLocale;
+  }
+
+  const navigatorLang = extractLanguage(navigatorLocale);
+
+  if (navigatorLocale.split('-').length > 1) {
+    if (availableLocales.includes(navigatorLang)) {
+      return navigatorLang;
+    }
+  }
+
+  if (availableLocales.includes(navigatorLang)) {
+    return navigatorLang;
+  }
+
+  const fallback = availableLocales.find(
+    locale => extractLanguage(locale) === navigatorLang
+  );
+
+  return fallback ? fallback : DEFAULT_LOCALE;
 }
 
 export default createI18n({
